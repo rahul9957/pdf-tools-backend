663a1f5d3307981830ddee2318f486a32bf9e081
rahul9957 <rahulkarton@gmail.com>
2025-08-03 23:46:15 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-exec("gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -r144 -sOutputFile=output.jpg input.pdf", (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Ghostscript error: ${error}`);
-        return;
-    }
-    console.log('Conversion done!');
-});
